90709628c5274a384cae58a7223307a2c8019a88
xlm1999 <xlm_1999@163.com>
2020-09-01 18:38:55 +0800

    update file

--- src/test/java/com/meng/springbootdemo/MyTest.java
+++ src/test/java/com/meng/springbootdemo/MyTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class MyTest {
     public static void main(String[] args) {
         List list = new ArrayList();
+
         System.out.println(list.get(0).toString() + "121212");
     }
 }
